086d32cb11748b500b9546ca1393cd2995be7318
azevedoedison <azevedoedison@gmail.com>
2021-08-27 07:42:36 -0300

    Alteração de package

--- exemplo.maven/src/main/java/br/com/dio/exemploMaven/Application.java
+++ exemplo.maven/src/main/java/br/com/dio/exemploMaven/Application.java
@@ -1,4 +1,4 @@
-package br.com.dio.exemplo.maven;
+package br.com.dio.exemploMaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
