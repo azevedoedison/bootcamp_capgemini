9ae9eaeacef91fc2c135acb254f50dd9ce78e605
azevedoedison <azevedoedison@gmail.com>
2021-09-28 10:37:10 -0300

    first commit

---
+++ reactive-programming/README.md
@@ -0,0 +1,1 @@
+Projeto para estudar conceitos de programação reativa utlizando RxJava e Java Reactive apresentados no curso de Programação reativa com Spring Web Flux
---
+++ reactive-programming/src/main/java/br/com/digitalinnovation/PrintReactive.java
@@ -0,0 +1,23 @@
+package br.com.digitalinnovation;
+ 
+
+import io.reactivex.rxjava3.core.Flowable;
+
+import java.util.concurrent.TimeUnit;
+
+public class PrintReactive {
+
+    public static void main(String... args) throws InterruptedException {
+
+        io.reactivex.rxjava3.core.
+
+        Flowable.interval( 1 , 1 , TimeUnit.SECONDS)
+                .map( n -> n * 2 )
+                .subscribe(System.out::println);
+
+        TimeUnit.SECONDS.sleep(5);
+
+    }
+
+
+}
